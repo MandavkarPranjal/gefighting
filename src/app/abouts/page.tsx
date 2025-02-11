867207bb05ed353e97044e0252f70bfc625925d9
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-02-11 23:16:31 +0530

    Added Metadata and Refactored code

--- src/app/abouts/page.tsx
+++ src/app/abouts/page.tsx
@@ -1,13 +1,21 @@
+import { type Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'About GeFighting',
+    description: 'Learn more about GeFighting - your premier destination for esports.',
+}
+
 
 export default function AboutPage() {
     return (
         <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-white dark:bg-neutral-950">
-            <div className="text-5xl sm:text-7xl md:text-8xl font-bold mb-8 tracking-tighter text-geblue/80 dark:text-geblue">
+            <h1 className="sm:text-7xl md:text-8xl text-9xl font-bold mb-8 tracking-tighter text-geblue/80 dark:text-geblue">
                 <span className="inline-block mr-4 last:mr-0">About</span>
                 <span className="inline-block mr-4 last:mr-0">
                     <span className="text-gered/85">Ge</span>Fighting
                 </span>
-            </div>
+            </h1>
+            {/* I want to make */}
         </div >
     )
 }
