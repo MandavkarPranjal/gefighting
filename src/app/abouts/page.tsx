2f4ad094f2143c2add60cd82c3c780f124ac0470
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-02-11 22:51:48 +0530

    New about page

---
+++ src/app/abouts/page.tsx
@@ -0,0 +1,13 @@
+
+export default function AboutPage() {
+    return (
+        <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-white dark:bg-neutral-950">
+            <div className="text-5xl sm:text-7xl md:text-8xl font-bold mb-8 tracking-tighter text-geblue/80 dark:text-geblue">
+                <span className="inline-block mr-4 last:mr-0">About</span>
+                <span className="inline-block mr-4 last:mr-0">
+                    <span className="text-gered/85">Ge</span>Fighting
+                </span>
+            </div>
+        </div >
+    )
+}
