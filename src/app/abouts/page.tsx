46e6898096aeb44dfd50fcf31d21bae1e735eacd
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-02-15 21:46:20 +0530

    small text for mobile

--- src/app/abouts/page.tsx
+++ src/app/abouts/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function AboutPage() {
     return (
         <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-white dark:bg-neutral-950">
-            <h1 className="sm:text-6xl md:text-7xl text-8xl font-bold mb-8 tracking-tighter text-geblue/80 dark:text-geblue">
+            <h1 className="sm:text-5xl md:text-7xl text-8xl font-bold mb-8 tracking-tighter text-geblue/80 dark:text-geblue">
                 <span className="inline-block mr-4 last:mr-0">About</span>
                 <span className="inline-block mr-4 last:mr-0">
                     <span className="text-gered/85">Ge</span>Fighting
